64683998e050da835638610b6a46418250f833b6
Pablo Y. Prado <93798700+pyurips@users.noreply.github.com>
2021-11-15 13:51:09 -0300

    The main file that will run the software.

---
+++ main.py
@@ -0,0 +1,43 @@
+# Importação de módulos ou pacotes #
+import matplotlib.pyplot as plt
+import time as t
+import os
+####################################
+
+# Variáveis #
+option = ""
+
+#############
+
+
+# Funções para a execução do menu #
+def menu():
+    print("=-=" * 10)
+    print("Digite |L| para exibir gráfico de linhas.")
+    print("Digite |B| para exibir gráfico de barras.")
+    print("Digite |X| para exibir gráfico de boxplot.")
+    print("Digite |T| para a resposta textual.")
+    print("Digite |A| para a imprimir arquivo.")
+    print("Digite |Q| para sair do programa.")
+    print("=-=" * 10)
+    option = input("=> ")
+    option = option.lower()
+    if (option != 'a' and option != 'b' and option != 'l' and option != 'x'
+            and option != 'q' and option != 't'):
+        os.system('clear')
+        print("Digite uma letra acima que seja válida!")
+        t.sleep(2)
+        os.system('clear')
+    if (option == 'q'):
+        os.system('clear')
+        print("Programa finalizado.")
+        t.sleep(1)
+        return "q"
+
+
+###################################
+
+# Execução do programa #
+while (option != "q"):
+    option = menu()
+########################
